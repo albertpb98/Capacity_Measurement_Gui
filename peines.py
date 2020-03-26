e129175951ac6f9fe68c549b46a4bb6c60f9e0f4
albertpb98 <apb1998@gmail.com>
2020-03-26 13:25:02 +0100

    I added comments

--- peines.py
+++ peines.py
@@ -13,13 +13,13 @@ sheet = openFile.sheet_by_name("59")
 #Cuidado con los cambios
 
 def getZ(number):
-    if number == 1:
+    if number == 1: # Esto es para 10Hz
      Z = sheet.col_values(2,415,616)
      print(Z)
-    elif number == 2:
+    elif number == 2: #Esto es para 1 Hz
      Z = sheet.col_values(9,415,616) 
     
-    elif number == 3:
+    elif number == 3: # Esto es para 0.1Hz
      Z = sheet.col_values(15,415,616)
     else:
      print ("El numero introducido es incorrecto!")
@@ -28,13 +28,13 @@ def getZ(number):
          
 
 def getPh(number):
-    if number == 1:
+    if number == 1: # Esto es para 10Hz
      Ph = sheet.col_values(3,415,616)
      
-    elif number == 2:
+    elif number == 2: # Esto es para 1 Hz
      Ph = sheet.col_values(10,415,616) 
     
-    elif number == 3:
+    elif number == 3: # Esto es para 0.1Hz
      Ph = sheet.col_values(16,415,616)
     else:
      print ("El numero introducido es incorrecto!")
@@ -42,12 +42,12 @@ def getPh(number):
     return Ph 
 
 def getZ1(number):
-    if number == 1:
+    if number == 1: # Esto es para 10Hz
      Z1 = sheet.col_values(4,415,616)
-    elif number == 2:
+    elif number == 2: #Esto es para 1 Hz
      Z1 = sheet.col_values(11,415,616) 
     
-    elif number == 3:
+    elif number == 3: # Esto es para 0.1Hz
      Z1 = sheet.col_values(17,415,616)
     else:
         print ("El numero introducido es incorrecto!")
@@ -55,12 +55,12 @@ def getZ1(number):
     return Z1
 
 def getZ2(number):
-    if number == 1:
+    if number == 1: # Esto es para 10Hz
      Z2 = sheet.col_values(5,415,616)
-    elif number == 2:
+    elif number == 2: #Esto es para 1 Hz
      Z2 = sheet.col_values(12,415,616) 
     
-    elif number == 3:
+    elif number == 3: # Esto es para 0.1Hz
      Z2 = sheet.col_values(18,415,616)
     else:
         print ("El numero introducido es incorrecto!")
@@ -68,13 +68,13 @@ def getZ2(number):
     return Z2 
 
 def getF(number):
-    if number == 1:
+    if number == 1: # Esto es para 10Hz
      f = sheet.col_values(1,415,616)
      
-    elif number == 2:
+    elif number == 2: #Esto es para 1 Hz
      f = sheet.col_values(8,415,616) 
     
-    elif number == 3:
+    elif number == 3: # Esto es para 0.1Hz
      f = sheet.col_values(14,415,616)
     else:
         print ("El numero introducido es incorrecto!")
@@ -82,13 +82,13 @@ def getF(number):
     return f 
 
 def getC(number):
-    if number == 1:
+    if number == 1: # Esto es para 10Hz
      C = sheet.col_values(6,415,616)
      
-    elif number == 2:
+    elif number == 2: # Esto es para 1 Hz
      C = sheet.col_values(13,415,616) 
     
-    elif number == 3:
+    elif number == 3: # Esto es para 0.1Hz
      C = sheet.col_values(19,415,616)
     else:
         print ("El numero introducido es incorrecto!")
